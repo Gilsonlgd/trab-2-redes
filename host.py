7e21ea17d89a1ca8d491f7efa7b93676b5a8cc96
Gilson Garcia <garciadasilvagilson@gmail.com>
2023-12-11 23:02:32 -0300

    ✨ feat: Update host script

--- host.py
+++ host.py
@@ -1,19 +1,32 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import OVSBridge
-from mininet.node import Node
-from mininet.log import setLogLevel, info
-from mininet.cli import CLI
 from scapy.all import *
-from scapy.all import sniff
 import threading
+import requests  # Importe a biblioteca requests
+from mininet.cli import CLI
 
-def callbackFunction(packet):
-    try:
-        print(packet.summary())
-    except KeyboardInterrupt:
-        pass
+def send_http_request(server_ip, message):
+    url = f'http://{server_ip}'
+    data = {'message': message}
+    response = requests.post(url, data=data)
+    print(f'Resposta do servidor: {response.text}')
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
-    sniff(prn=callbackFunction)
+    # Endereço IP do servidor
+    server_ip = '8.8.8.8'
+    
+    while True:
+        user_input = input("Digite a mensagem a ser enviada para o servidor (ou 'exit' para sair): ")
+        print(user_input)
+
+        # Verifique se o usuário deseja sair
+        if user_input.lower() == 'exit':
+            break
+
+        # Envie a mensagem para o servidor como uma requisição HTTP POST
+        send_http_request(server_ip, user_input)
+    exit(0)
+
+    # Pare a thread de captura quando o usuário sair
+    capture_thread.join()
+
